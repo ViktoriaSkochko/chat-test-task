c206371bf930d45498be3f7ef500f2333fbd249d
Viktoriia Skochko <skochko.viktoriia@gmail.com>
2020-08-05 13:19:26 +0300

    create user component

---
+++ src/app/sidebar/sidebar-user-item/sidebar-user-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-user-item',
+  templateUrl: './sidebar-user-item.component.html',
+  styleUrls: ['./sidebar-user-item.component.scss']
+})
+export class SidebarUserItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
